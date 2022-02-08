4e122b8cb12a7c906baec68ff83ef2d183aac1ad
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-02-08 17:47:26 +0300

    Initial commit

---
+++ js/preloader.js
@@ -0,0 +1,9 @@
+// анимация плавного скрытия прелоадера
+
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+    	document.body.classList.add('loaded');
+    	document.body.classList.remove('loaded_hiding');
+    }, 300);
+}

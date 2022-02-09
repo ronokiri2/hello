948bb1b5b337044cda1c8bec45588c0c260204f7
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-02-09 13:59:22 +0300

    js preloader now hides faster
    bugfix preloader hiding time
    before: 90 seconds
    after: 13 seconds

--- js/preloader.js
+++ js/preloader.js
@@ -1,9 +1,20 @@
 // анимация плавного скрытия прелоадера
 
-window.onload = function () {
-    document.body.classList.add('loaded_hiding');
-    window.setTimeout(function () {
-    	document.body.classList.add('loaded');
-    	document.body.classList.remove('loaded_hiding');
-    }, 300);
-}
+document.addEventListener('DOMContentLoaded', function () {
+	document.body.classList.add('loaded_hiding');
+	window.setTimeout(function () {
+	  document.body.classList.add('loaded');
+	  document.body.classList.remove('loaded_hiding');
+	}, 500);
+});
+
+// этот код на мобильных вызывал неприятный баг
+// страница отображалась только через полторы минуты
+// из-за медленной загрузки картинок
+// window.onload = function () {
+//     document.body.classList.add('loaded_hiding');
+//     window.setTimeout(function () {
+//     	document.body.classList.add('loaded');
+//     	document.body.classList.remove('loaded_hiding');
+//     }, 300);
+// }

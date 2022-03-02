876e93f4371dc2082b18ef1e256bbf3f1793945b
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-03-02 16:12:49 +0300

    gsap animation s2

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -1,45 +1,61 @@
-gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.matchMedia({
+	"(min-width: 700px)"() {
 
-// gsap.to(".s2__svg", {
-// 	scrollTrigger: ".s2__svg",
-// 	x: 400,
-// 	start: "top top",
-// 	rotation: 360,
-// 	duration: 3
-// });
-const vh = (coef) => window.innerHeight * (coef/100);
-const vw = (coef) => window.innerWidth * (coef/100);
+		gsap.registerPlugin(ScrollTrigger);
 
-const tl = gsap.timeline({
-	scrollTrigger: {
-	  trigger: ".s2__svg-perehod",
-	  start: "center center",
-	//   end: vh(200) + ' top',
-	  end: "+=200%",
-	  scrub: true,
-	  pin: true
+		// gsap.to(".s2__svg", {
+		// 	scrollTrigger: ".s2__svg",
+		// 	x: 400,
+		// 	start: "top top",
+		// 	rotation: 360,
+		// 	duration: 3
+		// });
+		const vh = (coef) => window.innerHeight * (coef/100);
+		const vw = (coef) => window.innerWidth * (coef/100);
+		
+		const tl = gsap.timeline({
+			scrollTrigger: {
+			  trigger: ".s2__svg-perehod",
+			  start: "center center",
+			//   end: vh(200) + ' top',
+			  end: "+=200%",
+			  scrub: true,
+			  pin: true
+			}
+		  });
+		
+		// Увеличение svg в 130 раз по кастомному изингу
+		tl.to(".s2__svg-perehod", {
+			scale: 130, 
+			duration: 1, 
+			ease: CustomEase.create("custom", "M0,0,C0.486,0.054,0.234,0.2,0.49,0.488,0.686,0.709,0.78,1,1,1"),
+		});
+		
+		
+		// Меняет цвет бэкраунда s2 на черный
+		// черный в css не установлен на случай если не загрузится js или сломается
+		gsap.to(".s2", {
+			backgroundColor: "black",
+			scrollTrigger: {
+			trigger: ".s1",
+			  start: "top top",
+			  end: "+=1%",
+			  scrub: true
+			}
+		  });
+		
+		
+		
+		// background color animation
+		gsap.to(".s2", {
+			backgroundColor: "white",
+			scrollTrigger: {
+			trigger: ".s2",
+			  start: "top top-=600px",
+			  end: "+=40%",
+			  scrub: true
+			}
+		  });
 	}
-  });
+})
 
-tl.to(".s2__svg-perehod", {scale: 130, duration: 1});
-
-
-gsap.to(".s2", {
-	backgroundColor: "black",
-	scrollTrigger: {
-	trigger: ".s1",
-	  start: "top top",
-	  end: "+=10%",
-	  scrub: true
-	}
-  });
-// background color animation
-gsap.to(".s2", {
-	backgroundColor: "white",
-	scrollTrigger: {
-	trigger: ".s2",
-	  start: "top top",
-	  end: "+=40%",
-	  scrub: true
-	}
-  });

0e725531678417e3295820a53b97624e4935aeec
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-03-03 17:28:48 +0300

    image optimisation, gsap s2 fix, gsap animation s3 added

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -12,7 +12,27 @@ ScrollTrigger.matchMedia({
 		// });
 		const vh = (coef) => window.innerHeight * (coef/100);
 		const vw = (coef) => window.innerWidth * (coef/100);
+
+				const tl = gsap.timeline({
+			scrollTrigger: {
+			  trigger: ".s2__svg-perehod",
+			  start: "center center",
+			//   end: vh(200) + ' top',
+			  end: "+=200%",
+			  scrub: true,
+			  pin: true
+			}
+		  });
 		
+		// Увеличение svg в 130 раз по кастомному изингу
+		tl.to(".s2__svg-perehod", {
+			scale: 130, 
+			duration: 1, 
+			ease: CustomEase.create("custom", "M0,0,C0.486,0.054,0.234,0.2,0.49,0.488,0.686,0.709,0.78,1,1,1"),
+		});
+		
+		
+		// анимация s1
 		gsap.from(".header", {
 			opacity: 0,
 			delay: 1
@@ -38,27 +58,9 @@ ScrollTrigger.matchMedia({
 			delay: 1
 		});
 
-		const tl = gsap.timeline({
-			scrollTrigger: {
-			  trigger: ".s2__svg-perehod",
-			  start: "center center",
-			//   end: vh(200) + ' top',
-			  end: "+=200%",
-			  scrub: true,
-			  pin: true
-			}
-		  });
-		
-		// Увеличение svg в 130 раз по кастомному изингу
-		tl.to(".s2__svg-perehod", {
-			scale: 130, 
-			duration: 1, 
-			ease: CustomEase.create("custom", "M0,0,C0.486,0.054,0.234,0.2,0.49,0.488,0.686,0.709,0.78,1,1,1"),
-		});
-		
-		
 		// Меняет цвет бэкраунда s2 на черный
 		// черный в css не установлен на случай если не загрузится js или сломается
+
 		gsap.to(".s2", {
 			backgroundColor: "black",
 			scrollTrigger: {
@@ -68,6 +70,11 @@ ScrollTrigger.matchMedia({
 				scrub: true
 			}
 		});
+
+
+
+		
+
 		
 		
 		
@@ -86,31 +93,38 @@ ScrollTrigger.matchMedia({
 		ScrollTrigger.create({
 			trigger: ".js1",
 			start: "top top", 
-			end: "+200%",
+			end: "223.4375%",
 			pin: ".js1"
 		});
 
 		ScrollTrigger.create({
 			trigger: ".js2",
 			start: "top top", 
-			end: "+200%",
+			end: "223.4375%",
 			pin: ".js2"
 		});
 
 		ScrollTrigger.create({
 			trigger: ".js3",
 			start: "top top", 
-			end: "+100%",
+			end: "111.71875%",
 			pin: ".js3"
 		});
 
 		ScrollTrigger.create({
 			trigger: ".js4",
 			start: "top top", 
-			end: "+100%",
+			end: "111.71875%",
 			pin: ".js4"
 		});
+			// 27.2135417vw
 
+			// 836px 1024px
+
+			// 122.488038278%
+
+			// 1024 px = 33.3333333vw
+			// margin 3.90625vw = 11.71875%
 		// ScrollTrigger.create({
 		// 	trigger: ".js5",
 		// 	start: "top top", 
@@ -127,20 +141,46 @@ ScrollTrigger.matchMedia({
 
 
 
-
-
-
-
-		gsap.to(".s9", {
-			scale: 0.8,
+		gsap.to(".first-row", {
+			xPercent: 30,
 			
 			scrollTrigger: {
-				trigger: ".footer",
+				trigger: ".first-row",
 				start: "top bottom",
-				// end: "+=1%",
 				scrub: true
 			}
 		});
+
+		gsap.to(".second-row", {
+			xPercent: -30,
+			
+			scrollTrigger: {
+				trigger: ".second-row",
+				start: "top bottom",
+				scrub: true
+			}
+		});
+
+		gsap.to(".third-row", {
+			xPercent: 30,
+			
+			scrollTrigger: {
+				trigger: ".third-row",
+				start: "top bottom",
+				scrub: true
+			}
+		});
+
+		// gsap.to(".s9", {
+		// 	scale: 0.8,
+			
+		// 	scrollTrigger: {
+		// 		trigger: ".footer",
+		// 		start: "top bottom",
+		// 		// end: "+=1%",
+		// 		scrub: true
+		// 	}
+		// });
 
 
 

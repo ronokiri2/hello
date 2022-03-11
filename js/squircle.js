f0462d75a2867384492b5dd16407a15105d1b54e
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-03-11 18:02:28 +0300

    added responsive squircle script for video and posters
    Обычные скругления углов заменены на скругления суперэллипсов (как у Apple, при том что радиусы углов не изменились) для видео на первом экране и постеров в секции KION ORIGINALS (видно только в современных версиях Google Chrome, потому что используется экспериментальная функция Houdini API, а другие браузеры пока не поддерживают Houdini API)
    
    Сайт написан в современном подходе, и не использует пиксели в качестве размеров элементов. Вместо пикселей используются проценты от ширины экрана. Вот только Houdini API может использовать только пиксели. Это приводит к тому, что на маленьких дисплеях углы будут плавными, а на больших дисплеях острыми.
    
    Поэтому я написал скрипт, который позволяет мне использовать проценты вместо пикселей.
    Но если изменить размер окна браузера с 4000 пикслей до 300 пикселей, то скрипт не узнает об этом, и будет считать, что размер окна остался большим.
    Поэтому я улучшил скрипт так, чтобы он переопределял значение в 1% ширины экрана, при изменение размера окна браузера.
    Теперь углы на сайте очень плавные, при том, что их радиус не изменился.

--- js/squircle.js
+++ js/squircle.js
@@ -7,23 +7,87 @@ if ("paintWorklet" in CSS) {
 		'js/houdini-squircle.min.js'
 	);
 
+	const s1__wrapper = document.querySelector(".s1__wrapper");
+	const video = document.querySelector(".s1__video");
 	const posters = document.querySelectorAll(".s2__posters--poster");
 
 	let vw = window.innerWidth / 100;
 	console.log(`Start vw: ${vw}px`);
 
-	for (const poster of posters) {
-		poster.style.setProperty('--squircle-radius', `${vw}`);
-	}
 
-	// замена радиуса при смена размера окна
-	window.addEventListener('resize', function(event) {
+	// при запуске сайта, один раз запустится проверка на разрешение экрана:
+	if (window.matchMedia("(min-width: 700px)").matches) {
+		// для больших дисплеев:
+		console.log(`Big display`);
+
 		let vw = window.innerWidth / 100;
-		console.log(`vw: ${vw}px`);
+		let videoRaius = 3.25520833 * vw;
 
+		// s1
+		s1__wrapper.style.setProperty('--squircle-radius', `${videoRaius}`);
+		video.style.setProperty('--squircle-radius', `${videoRaius}`);
+		
+		// s2 posters
 		for (const poster of posters) {
 			poster.style.setProperty('--squircle-radius', `${vw}`);
 		}
+
+		
+	} else {
+		// для смартфонов:
+		console.log(`Small display`);
+
+		let videoRaiusMobile = 12.5 * vw;
+		let posterRadiusMobile = 4.09375 * vw;
+
+		video.style.setProperty('--squircle-radius', `${videoRaiusMobile}`);
+		s1__wrapper.style.setProperty('--squircle-radius', `${videoRaiusMobile}`);
+	
+		for (const poster of posters) {
+			poster.style.setProperty('--squircle-radius', `${posterRadiusMobile}`);
+		}
+	}
+
+
+
+	// замена радиуса при смена размера окна
+	window.addEventListener('resize', function(event) {
+
+		let vw = window.innerWidth / 100;
+		console.log(`resized vw: ${vw}px`);
+
+
+		// для больших дисплеев
+		if (window.innerWidth >= 700) {
+			
+			let vw = window.innerWidth / 100;
+			let videoRaius = 3.25520833 * vw;
+
+			// s1
+			s1__wrapper.style.setProperty('--squircle-radius', `${videoRaius}`);
+			video.style.setProperty('--squircle-radius', `${videoRaius}`);
+			
+			// s2 posters
+			for (const poster of posters) {
+				poster.style.setProperty('--squircle-radius', `${vw}`);
+			}
+
+		} else {
+		// для смартфонов
+
+			let videoRaiusMobile = 12.5 * vw;
+			let posterRadiusMobile = 4.09375 * vw;
+
+			video.style.setProperty('--squircle-radius', `${videoRaiusMobile}`);
+			s1__wrapper.style.setProperty('--squircle-radius', `${videoRaiusMobile}`);
+		
+			for (const poster of posters) {
+				poster.style.setProperty('--squircle-radius', `${posterRadiusMobile}`);
+			}
+		}
+		
+
+
 	}, true);
 
 

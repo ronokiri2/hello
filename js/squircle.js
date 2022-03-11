02230fac3721b2487dd9ad835e138cebab5f9ce4
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-03-11 15:01:39 +0300

    added squircle for posters

--- js/squircle.js
+++ js/squircle.js
@@ -1,31 +1,33 @@
-// // Houdini squircle 
-// // Скругления суперэллипсов
-// // Не работает для тега <a>
+// Houdini squircle 
+// Скругления суперэллипсов
+// Не работает для тега <a>
 
-// if ("paintWorklet" in CSS) {
-// 	CSS.paintWorklet.addModule(
-// 		"https://www.unpkg.com/css-houdini-squircle@0.2.1/squircle.min.js"
-// 	);
+if ("paintWorklet" in CSS) {
+	CSS.paintWorklet.addModule(
+		'js/houdini-squircle.min.js'
+	);
 
-// 	const button = document.querySelector(".s1__button2");
+	const posters = document.querySelectorAll(".s2__posters--poster");
 
-// 	let vw = window.innerWidth / 100;
-// 	let buttonRadius = 0.325520833 * vw;
-// 	console.log(` early vw: ${vw}px`);
-// 	button.style.setProperty('--squircle-radius', `${buttonRadius}`);
+	let vw = window.innerWidth / 100;
+	console.log(`Start vw: ${vw}px`);
+
+	for (const poster of posters) {
+		poster.style.setProperty('--squircle-radius', `${vw}`);
+	}
+
+	// замена радиуса при смена размера окна
+	window.addEventListener('resize', function(event) {
+		let vw = window.innerWidth / 100;
+		console.log(`vw: ${vw}px`);
+
+		for (const poster of posters) {
+			poster.style.setProperty('--squircle-radius', `${vw}`);
+		}
+	}, true);
 
 
-
-// 	window.addEventListener('resize', function(event) {
-// 		let vw = window.innerWidth / 100;
-// 		let buttonRadius = 0.325520833 * vw;
-// 		console.log(`vw: ${vw}px`);
-// 		// button.style = `--squircle-radius: calc(0.325520833 * ${vw})px;`
-// 		button.style.setProperty('--squircle-radius', `${buttonRadius}`);
-// 	}, true);
-
-
-// }
+}
 
 
 

0028958d3aa52b93d1562c4f82876cbb333c893a
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-03-21 15:38:55 +0300

    added squircle border radius for s3
    — Добавлены скругления суперэллипсов для постеров на 3 экране сайта

--- js/squircle.js
+++ js/squircle.js
@@ -11,17 +11,21 @@ if ("paintWorklet" in CSS) {
 	const video = document.querySelector(".s1__video");
 	const posters = document.querySelectorAll(".s2__posters--poster");
 
+	const s3_images = document.querySelectorAll(".s3__images--image");
+
 	let vw = window.innerWidth / 100;
 	console.log(`Start vw: ${vw}px`);
 
 
 	// при запуске сайта, один раз запустится проверка на разрешение экрана:
 	if (window.matchMedia("(min-width: 700px)").matches) {
+
 		// для больших дисплеев:
 		console.log(`Big display`);
 
 		let vw = window.innerWidth / 100;
 		let videoRaius = 3.25520833 * vw;
+		let s3_imageRadius = 0.651041667 * vw;
 
 		// s1
 		s1__wrapper.style.setProperty('--squircle-radius', `${videoRaius}`);
@@ -32,19 +36,33 @@ if ("paintWorklet" in CSS) {
 			poster.style.setProperty('--squircle-radius', `${vw}`);
 		}
 
+		// s3 images
+		for (const s3_image of s3_images) {
+			s3_image.style.setProperty('--squircle-radius', `${s3_imageRadius}`);
+		}
+
 		
 	} else {
+
 		// для смартфонов:
 		console.log(`Small display`);
 
 		let videoRaiusMobile = 12.5 * vw;
 		let posterRadiusMobile = 4.09375 * vw;
+		let s3_imageRadiusMobile = 1.25 * vw;
 
-		video.style.setProperty('--squircle-radius', `${videoRaiusMobile}`);
+		// s1
 		s1__wrapper.style.setProperty('--squircle-radius', `${videoRaiusMobile}`);
+		video.style.setProperty('--squircle-radius', `${videoRaiusMobile}`);
 	
+		// s2 posters
 		for (const poster of posters) {
 			poster.style.setProperty('--squircle-radius', `${posterRadiusMobile}`);
+		}
+
+		// s3 images
+		for (const s3_image of s3_images) {
+			s3_image.style.setProperty('--squircle-radius', `${s3_imageRadiusMobile}`);
 		}
 	}
 
@@ -62,6 +80,7 @@ if ("paintWorklet" in CSS) {
 			
 			let vw = window.innerWidth / 100;
 			let videoRaius = 3.25520833 * vw;
+			let s3_imageRadius = 0.651041667 * vw;
 
 			// s1
 			s1__wrapper.style.setProperty('--squircle-radius', `${videoRaius}`);
@@ -72,17 +91,30 @@ if ("paintWorklet" in CSS) {
 				poster.style.setProperty('--squircle-radius', `${vw}`);
 			}
 
+			// s3 images
+			for (const s3_image of s3_images) {
+				s3_image.style.setProperty('--squircle-radius', `${s3_imageRadius}`);
+			}
+
 		} else {
 		// для смартфонов
 
 			let videoRaiusMobile = 12.5 * vw;
 			let posterRadiusMobile = 4.09375 * vw;
+			let s3_imageRadiusMobile = 1.25 * vw;
 
+			// s1 mobile
 			video.style.setProperty('--squircle-radius', `${videoRaiusMobile}`);
 			s1__wrapper.style.setProperty('--squircle-radius', `${videoRaiusMobile}`);
 		
+			// s2 posters mobile
 			for (const poster of posters) {
 				poster.style.setProperty('--squircle-radius', `${posterRadiusMobile}`);
+			}
+
+			// s3 images mobile
+			for (const s3_image of s3_images) {
+				s3_image.style.setProperty('--squircle-radius', `${s3_imageRadiusMobile}`);
 			}
 		}
 		

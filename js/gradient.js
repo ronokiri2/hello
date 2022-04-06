dc326316e54afd337afa418a09fb88b2ef11e3ce
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-04-06 18:11:26 +0300

    added canvas gradient footer animation

---
+++ js/gradient.js
@@ -0,0 +1,29 @@
+var granimInstance = new Granim({
+    element: '.canvas',
+    direction: 'custom',
+	customDirection: {
+		x0: '0%',
+		y0: '100%',
+		x1: '100%',
+		y1: '0%'
+	},
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+				[
+					{ color: '#3A3D91', pos: 0 },
+					{ color: '#C42E72', pos: .5 }
+				],
+				[
+					{ color: '#2575fc', pos: 0 },
+					{ color: '#912CFF', pos: .5 }
+				],
+				[
+					{ color: '#524EFF', pos: 0 },
+					{ color: '#FF3995', pos: .5 }
+				]
+            ]
+        }
+    }
+});

1f61496e51ff5902cab552bca3e7956d80daa16f
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-04-18 14:58:11 +0300

    added scrollock for preloader

--- js/preloader.js
+++ js/preloader.js
@@ -10,8 +10,65 @@
 
 // }, true);
 
+
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+// этот код на мобильных вызывал неприятный баг
+// страница отображалась только через полторы минуты
+// из-за медленной загрузки картинок
+// window.onload = function () {
+//     document.body.classList.add('loaded_hiding');
+//     window.setTimeout(function () {
+//     	document.body.classList.add('loaded');
+//     	document.body.classList.remove('loaded_hiding');
+//     }, 300);
+// }
+
+disableScroll();
+
 document.addEventListener('DOMContentLoaded', function () {
 	function timer() {
+		enableScroll();
 		document.body.classList.add('loaded_hiding');
 		document.body.classList.remove('js-scroll-lock'); // разблокировать скролл после скрытия прелоадера
 
@@ -23,14 +80,3 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 	setTimeout(timer, 2000);
 });
-
-// этот код на мобильных вызывал неприятный баг
-// страница отображалась только через полторы минуты
-// из-за медленной загрузки картинок
-// window.onload = function () {
-//     document.body.classList.add('loaded_hiding');
-//     window.setTimeout(function () {
-//     	document.body.classList.add('loaded');
-//     	document.body.classList.remove('loaded_hiding');
-//     }, 300);
-// }

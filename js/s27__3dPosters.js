58936a91a70967d20866c3752e928864f3bbb3be
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-06-06 17:19:40 +0300

    GSAP to Motion one

--- js/s27__3dPosters.js
+++ js/s27__3dPosters.js
@@ -1,5 +1,5 @@
 // Elements
-var scene1 = document.getElementById('scene1');  
+var scene1 = document.getElementById('scene1'); 
 var scene2 = document.getElementById('scene2');
 var scene3 = document.getElementById('scene3');  
 var scene4 = document.getElementById('scene4');
--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -18,64 +18,6 @@ const vw = (coef) => window.innerWidth * (coef/100);
 
 
 
-// анимация плавного скрытия прелоадера с блокировкой скролла
-
-// left: 37, up: 38, right: 39, down: 40,
-// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-
-function preventDefault(e) {
-  e.preventDefault();
-}
-
-function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
-}
-
-// modern Chrome requires { passive: false } when adding event
-var supportsPassive = false;
-try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-  }));
-} catch(e) {}
-
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-// call this to Disable
-function disableScroll() {
-  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-// call this to Enable
-function enableScroll() {
-  window.removeEventListener('DOMMouseScroll', preventDefault, false);
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-  window.removeEventListener('touchmove', preventDefault, wheelOpt);
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-window.scrollTo( 0, 0 );
-disableScroll();
-
-gsap.to(".preloader", {
-	opacity: 0,
-	duration: 0.2,
-	display: "none",
-	delay: 2,
-	onComplete: enableScroll2
-});
-
-function enableScroll2() {
-	enableScroll();
-}
 
 // Увеличение svg в 130 раз
 gsap.to(".overlay2", {
@@ -90,28 +32,28 @@ gsap.to(".overlay2", {
 });
 
 // анимация s1
-gsap.from(".header", {
-	opacity: 0,
-	delay: 2.8,
-});
-gsap.from(".s1__wrapper", {
-	scale: 0.272197962,
-	duration: 1,
-	delay: 2,
-	ease: "circ.out"
-});
-gsap.from(".s1__h1", {
-	opacity: 0,
-	delay: 2.5
-});
-gsap.from(".s1__h2", {
-	opacity: 0,
-	delay: 2.5
-});		
-gsap.from(".s1__button", {
-	opacity: 0,
-	delay: 2.5
-});
+// gsap.from(".header", {
+// 	opacity: 0,
+// 	delay: 2.8,
+// });
+// gsap.from(".s1__wrapper", {
+// 	scale: 0.272197962,
+// 	duration: 1,
+// 	delay: 2,
+// 	ease: "circ.out"
+// });
+// gsap.from(".s1__h1", {
+// 	opacity: 0,
+// 	delay: 2.5
+// });
+// gsap.from(".s1__h2", {
+// 	opacity: 0,
+// 	delay: 2.5
+// });		
+// gsap.from(".s1__button", {
+// 	opacity: 0,
+// 	delay: 2.5
+// });
 
 
 
